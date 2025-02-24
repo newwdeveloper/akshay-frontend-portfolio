2900ab50cc1a470877d47126838065863a178156
Akshay <akshaymangutkar951@gmail.com>
2025-02-24 17:38:42 +0530

    added toggle for navbar link, and provided the styling for smaller screen

--- src/Components/navbar/NavbarMain.jsx
+++ src/Components/navbar/NavbarMain.jsx
@@ -1,14 +1,31 @@
+import { useState } from "react";
 import NavbarBtn from "./NavbarBtn";
 import NavbarLinks from "./NavbarLinks";
 import NavbarLogo from "./NavbarLogo";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 const NavbarMain = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const handleToggle = () => {
+    setMenuOpen(!menuOpen);
+  };
   return (
     <nav className="max-w-[1300px] mx-auto px-4 w-full fixed left-[50%] -translate-x-[50%] z-20 flex gap-4 mt-2">
       <div className="flex justify-between w-full max-w-[1200px] mx-auto bg-black items-center p-6 rounded-r-full rounded-l-full border-[0.5px] border-orange">
         <NavbarLogo />
-        <NavbarLinks />
+        <div className={`${menuOpen ? "sm:block" : "sm:hidden"} lg:block`}>
+          <NavbarLinks />
+        </div>
+
         <NavbarBtn />
+      </div>
+      <div className="flex lg:hidden sm:block p-6 bg-black items-center justify-center rounded-full  border-orange border-[0.5px]">
+        <button
+          className="text-2xl p-3 border border-orange rounded-full text-white"
+          onClick={handleToggle}
+        >
+          <GiHamburgerMenu />
+        </button>
       </div>
     </nav>
   );
