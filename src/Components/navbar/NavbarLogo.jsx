bdc9135ac515eef6a7f7bb8f96c95ca602a766df
Akshay <akshaymangutkar951@gmail.com>
2025-02-24 18:10:49 +0530

    completed the navbar styling and responsiveness

--- src/Components/navbar/NavbarLogo.jsx
+++ src/Components/navbar/NavbarLogo.jsx
@@ -1,7 +1,12 @@
 const NavbarLogo = () => {
   return (
     <div>
-      <h1 className="text-white text-2xl font-body ">Akshay</h1>
+      <h1 className="text-white text-2xl font-body sm:hidden md:block ">
+        Akshay
+      </h1>
+      <h1 className="text-white text-4xl font-special font-extrabold sm:block md:hidden ">
+        A
+      </h1>
     </div>
   );
 };
